6f79882ce710c57d76889d39d01f7f08afd73e27
Aaron Den Hond <aaron.denhond@gmail.com>
2021-07-14 11:07:13 +0200

    step 15

--- src/app/add-friend.service.ts
+++ src/app/add-friend.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Friend } from './friend';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AddFriendService {
-  private http: HttpClient;
-  constructor(http: HttpClient) {
-    this.http = http;
+  private url: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  addFriend(friend: Friend) {
+    return this.http.post(this.url, friend);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Friend } from './friend';
+import { AddFriendService } from './add-friend.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,18 @@ import { Friend } from './friend';
 export class AppComponent {
   languageOptions = ['PHP', 'Javascript', 'C', 'Java', 'Ruby'];
   friendModel = new Friend('', '', '', 0, '');
-  userData() {
+  constructor(private addFriendService: AddFriendService) {}
+  onSubmit() {
     console.log(this.friendModel);
+    this.addFriendService
+      .addFriend(this.friendModel)
+      //input field bound to friendModel
+      .subscribe(
+        (data) => console.log('it works', data),
+        (error) => console.log("it doesn't work", error)
+      );
   }
 }
+
+//In your app.component.ts also import your service and make a constructor in the component class.
+//Add a private "addFriendService" property and typehint it.
